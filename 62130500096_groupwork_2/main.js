b0b78ef4054d1a974cb4ddf528446be99b6a46e7
similanklinsmith <57857644+similanklinsmith@users.noreply.github.com>
2021-02-03 10:05:42 +0700

    Delete 62130500096_groupwork_2 directory

--- 62130500096_groupwork_2/main.js
+++
@@ -1,9 +0,0 @@
-    const app = {
-        data() {
-            return {
-                msg: 'Hello, Vue3'
-            }
-        }
-
-    }
-    const mountedApp = Vue.createApp(app).mount('#app')
